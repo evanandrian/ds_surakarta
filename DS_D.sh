6df79fc38b7386a441066c5cf1002ff4cc5392c2
agussustian2017 <agus.sustian.epic@gmail.com>
2019-11-18 06:23:11 +0700

    Revert "15.2"
    This reverts commit 30c2603ae1132eff2104f8a42a8bfaeaceeac234.
    
    # Conflicts:
    #	DesktopService.exe

--- DS_D.sh
+++ DS_D.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-"D:\ds_merahputih\kill_DS.exe" $1 &
+"D:\ds_surakarta\kill_DS.exe" $1 &
 git pull
-"D:\ds_merahputih\DesktopService.exe" $1 &
+"D:\ds_surakarta\DesktopService.exe" $1 &
 
